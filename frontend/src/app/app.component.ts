f28e0374538762e650e10276fb518239e911fefe
EhrlerL <leonid@ehrler-web.de>
2025-05-16 18:20:38 +0200

    implement card pick logic

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 
 import { NameInputComponent } from './name-input/name-input.component';
 import { PlayerComponent } from './player/player.component';
+import { CardPickerComponent } from './card-picker/card-picker.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NameInputComponent, PlayerComponent],
+  imports: [NameInputComponent, PlayerComponent, CardPickerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/src/app/card-picker/card-picker.component.ts
+++ frontend/src/app/card-picker/card-picker.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card-picker',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card-picker.component.html',
   styleUrl: './card-picker.component.css'
 })
 export class CardPickerComponent {
+  fibonacciButtons: string[] = ["1","2","3","5","8","13","21","34","55","89","?","∞","☕️"];
+  style: string = "btn-outline btn-primary";
+  selectedCard: string = "Keine";
 
+  handleClick(label: string) {
+    this.selectedCard = label;
+    console.log("Selected card:", this.selectedCard);
+    // Change Button Style
+    if (label === this.selectedCard) {
+      this.style = "btn-primary";
+    }
+  }
 }
