09fbee93de17e8a861b11a305b85246cc38f87c8
EhrlerL <leonid@ehrler-web.de>
2025-05-16 16:28:08 +0200

    implement name input modal

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+//import { RouterOutlet } from '@angular/router';
+
+import { NameInputComponent } from './name-input/name-input.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NameInputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
