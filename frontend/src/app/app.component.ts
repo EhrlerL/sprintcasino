e78e51963a38b87de3cf8a31b393cd3eede60598
EhrlerL <leonid@ehrler-web.de>
2025-05-16 17:44:48 +0200

    display player on main screen

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 //import { RouterOutlet } from '@angular/router';
 
 import { NameInputComponent } from './name-input/name-input.component';
+import { PlayerComponent } from './player/player.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NameInputComponent],
+  imports: [NameInputComponent, PlayerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
