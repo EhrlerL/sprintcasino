a386d6441991f9ea3ae790db52cca5e3c5c69b3f
EhrlerL <leonid@ehrler-web.de>
2025-05-16 17:50:28 +0200

    generate card picker component

---
+++ frontend/src/app/card-picker/card-picker.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-picker',
+  imports: [],
+  templateUrl: './card-picker.component.html',
+  styleUrl: './card-picker.component.css'
+})
+export class CardPickerComponent {
+
+}
