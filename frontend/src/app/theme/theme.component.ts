007c53f15d6af584c4397f14a24e98e599cec18c
EhrlerL <leonid@ehrler-web.de>
2025-05-21 09:35:12 +0200

    prettify theme selector

--- frontend/src/app/theme/theme.component.ts
+++ frontend/src/app/theme/theme.component.ts
@@ -1,26 +1,42 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+interface Theme {
+  name: string;
+  value: string;
+}
+
 @Component({
   selector: 'app-theme',
   imports: [CommonModule],
   templateUrl: './theme.component.html',
   styleUrl: './theme.component.css'
 })
+
 export class ThemeComponent {
 
-  themes: string[] = ["light", "dark", "caramellatte", "coffee", "retro", "forest", "aqua", "cyberpunk", "valentine"];
+  themes: Theme[] = [
+    {name: "☀", value: "light"}, 
+    {name: "🌙", value: "dark"}, 
+    {name:"🥛", value: "caramellatte"}, 
+    {name: "☕️", value: "coffee"}, 
+    {name: "📺", value: "retro"}, 
+    {name: "🥰", value: "valentine"}
+  ];
+  currentTheme: Theme = this.themes[0];
 
   // save theme in local storage and set theme for site
   setTheme(theme: string) {
-    localStorage.setItem('theme', theme);
     document.body.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+    this.currentTheme = this.themes.find(t => t.value == theme) || this.themes[0];
   }
 
   ngOnInit() {
     const theme = localStorage.getItem('theme');
     if (theme) {
-      document.body.setAttribute('data-theme', theme);
+      document.body.setAttribute('data-theme', theme || 'light');
+      this.currentTheme = this.themes.find(t => t.value == theme) || this.themes[0];
     }
   }
 }
